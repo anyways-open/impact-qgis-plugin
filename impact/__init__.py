571a8a019a1d97c9985219597c0bb6fb44620123
pietervdvn <pietervdvn@posteo.net>
2021-11-08 16:43:54 +0100

    Add refactored plugin

---
+++ impact/__init__.py
@@ -0,0 +1,285 @@
+import json
+from urllib import request
+
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QColor
+from PyQt5.QtNetwork import QNetworkReply, QNetworkRequest
+from qgis.core import *
+
+standalone_mode = False
+staging_mode = True
+
+def setStandalone():
+    """
+    Changes the behaviour of 'fetch_blocking' and 'fetch_non_blocking' to use builin python libs instead of QGis
+    :return: 
+    """
+    global standalone_mode
+    standalone_mode = True
+
+
+def fetch_blocking(url):
+    """
+    Gets a network resource in a synchronous way.
+    If 'setStandalone' is called, uses urllib.request
+    Otherwise, a QNetworkRequest and GgsBlockingNetworkRequest is used.
+    
+    :return: a string with the contents. Throws an exception if the resource could not be read
+    """
+
+    if standalone_mode:
+        return request.urlopen(url).read().decode("UTF-8")
+
+    blockingRequest = QgsBlockingNetworkRequest()
+    result = blockingRequest.get(QNetworkRequest(QUrl(url)))
+
+    if result != QgsBlockingNetworkRequest.NoError:
+        raise Exception("Could not download " + url)
+
+    # reply: QgsNetworkReplyContent
+    reply = blockingRequest.reply()
+
+    if reply.error() != QNetworkReply.NoError:
+        raise Exception("Could not download " + url + " due to " + reply.errorString())
+
+    qbyteArray = reply.content()
+    return bytes(qbyteArray).decode("UTF-8")
+
+
+all_callbacks = set()
+
+
+def fetch_non_blocking(url, callback, onerror, postData=None, headers=None):
+    """
+    
+    Fetches the requested url in a non-blocking way.
+    When the data is fetched, the callback will be called with the response as single string.
+    
+    If postData is defined as object, then the header 'Content-Type=application/json' will be automatically set.
+    The postData will be automatically serialized as JSON
+    
+    Note: will be _blocking_ in standalone mode too!
+    :return: 
+    """
+
+    if headers is None:
+        headers = {}
+
+    if callback is None:
+        raise Exception("No callback given for fetch_non_blocking")
+
+    if postData is not None:
+        if "Content-Type" not in headers:
+            headers["Content-Type"] = "application/json"
+        QgsMessageLog.logMessage(
+            "POST-request to " + url + " with headers " + json.dumps(headers) + " and data" + json.dumps(postData),
+            'ImPact Toolbox', level=Qgis.Info)
+        print(
+            "POST-request to " + url + " with headers " + json.dumps(headers) + " and data" + json.dumps(postData))
+
+        req = request.Request(url, data=json.dumps(postData).encode('UTF-8'),
+                              headers=headers)  # this will make the method "POST"
+        resp = request.urlopen(req)
+        raw = resp.read().decode("UTF-8")
+        callback(raw)
+        return
+
+    if standalone_mode:
+        req = request.Request(url, headers=headers)
+        text = request.urlopen(req).read().decode("UTF-8")
+        callback(text)
+        return
+
+    fetcher = QgsNetworkContentFetcher()
+
+    def onFinished(self_function):
+        content = fetcher.contentAsString()
+        callback(content)
+        all_callbacks.remove(callback)
+        all_callbacks.remove(self_function)
+
+    all_callbacks.add(callback)
+    all_callbacks.add(onFinished)
+    fetcher.finished.connect(lambda: onFinished(onFinished))
+
+    req = QNetworkRequest(QUrl(url))
+    for header in headers.items():
+        req.setRawHeader(bytes(header[0], "UTF-8"), bytes(header[1], "UTF-8"))
+
+    fetcher.fetchContent(req)
+
+
+def extract_valid_geometries(iface, features, warning='The selected layer has some entries where the geometry is Null'):
+    """check if a list of feature has empty geometries. 
+    Args:
+        iface (QgisInterface): the interface with the mainWindow
+        featureLayer (QgsVectorLayer): the layer to check
+        warning (string, optional): a optional message to give to user if a null-geometry is found
+    Returns:
+        (features[]): all the features for which the geometry is valid
+    """
+    valid_features = []
+    fautly_features_count = 0
+    for feat in features:
+        if feat.geometry().isNull():
+            fautly_features_count += 1
+        else:
+            valid_features.append(feat)
+
+    if fautly_features_count > 0:
+        iface.messageBar().pushMessage('ImPact_toolbox Warning', warning, level=Qgis.Warning)
+
+    return valid_features
+
+
+def transform_layer_to_WGS84(layer):
+    """
+     Cheking and transforming layers' CRS to EPSG: 4326
+    :param layer: A qgis layer
+    :return: A list of features
+    """
+    if layer.crs() == 4326:
+        features = layer.getFeatures()
+    else:
+        crsSrc = QgsCoordinateReferenceSystem(layer.crs())
+        crsDest = QgsCoordinateReferenceSystem(4326)
+        xform = QgsCoordinateTransform(crsSrc, crsDest, QgsProject.instance())
+
+        features = []
+        for f in layer.getFeatures():
+            g = f.geometry()
+            g.transform(xform)
+            f.setGeometry(g)
+            features.append(f)
+    return features
+
+
+def extract_polygons(layer):
+    pass
+
+
+def lat_lon_coor(coor):
+    """
+    Reverses the coordinates
+    :param coor: 
+    :return: 
+    """
+    splitted = coor.split(",")
+    return splitted[1] + "," + splitted[0]
+
+
+def create_layergroup_from_files(filelist, groupname, color="#ff0000", width=1.0):
+    root = QgsProject.instance().layerTreeRoot()
+    shapeGroup = root.addGroup(groupname)  # Ater or Before (basically any name can be given to the group
+
+    for file in filelist:
+        try:
+            QgsMessageLog.logMessage("Creating a layer from file " + file, 'ImPact Toolbox', level=Qgis.Info)
+            fileroute = file
+            filename = QgsVectorLayer(fileroute, file[:-5], "ogr")
+            QgsProject.instance().addMapLayer(filename, False)
+            symbols = filename.renderer().symbols(QgsRenderContext())
+            sym = symbols[0]
+            sym.setColor(QColor(color))
+            sym.setWidth(float(width))
+            filename.triggerRepaint()
+            shapeGroup.insertChildNode(1, QgsLayerTreeLayer(filename))
+        except Exception as e:
+            QgsMessageLog.logMessage("Creating a layer from file " + file + " failed due to " + e.message,
+                                     'ImPact Toolbox', level=Qgis.Warning)
+
+
+def create_layer_from_file(iface, filename):
+    # filepath, name in qgis, type
+    lyr = QgsVectorLayer(filename, filename, "ogr")
+    QgsProject.instance().addMapLayer(lyr)
+    return lyr
+
+
+def extract_coordinates_array(features, latlonformat=False):
+    """
+    Returns the coordinates of the given features as a list containing two numbers, e.g. [[4.1,51.2], [4.2,51.3], ... ]
+    Returns [lon, lat] by default, unless 'latlonformat' is set
+    :param features: 
+    """
+    result = []
+    for feature in features:
+        if isinstance(feature, QgsPointXY):
+            point = feature
+        else:
+            point = feature.geometry().asPoint()
+        X = point.x()
+        Y = point.y()
+        coor = None
+        if latlonformat:
+            coor = [Y, X]
+        else:
+            coor = [X, Y]
+        result.append(coor)
+    return result
+
+
+def extract_coordinates(features, latlonformat=False):
+    """
+    Returns the coordinates of the given features as a list of strings such as "lon,lat"
+    :param features: 
+    """
+    result = []
+    for feature in features:
+        if isinstance(feature, QgsPointXY):
+            point = feature
+        else:
+            point = feature.geometry().asPoint()
+        X = point.x()
+        Y = point.y()
+        coor = None
+        if latlonformat:
+            coor = str(Y) + "," + str(X)
+        else:
+            coor = str(X) + ',' + str(Y)
+        result.append(coor)
+    return result
+
+
+def layer_as_geojson_features(iface, lyr):
+    line_features = extract_valid_geometries(iface, transform_layer_to_WGS84(lyr))
+    fields = list(map(lambda field: field.name(), lyr.fields()))
+    features = []
+    for qgsFeature in line_features:
+        props = {}
+        for fieldName in fields:
+            if fieldName == "":
+                continue
+            val = qgsFeature.attribute(fieldName)
+            if val == NULL:
+                continue
+            props[fieldName] = val
+        gqisPoints = qgsFeature.geometry().asPolyline()
+        coordinates = list(map(lambda qgisPoint: [qgisPoint[0], qgisPoint[1]], gqisPoints))
+        features.append({
+            "type": "Feature",
+            "properties": props,
+            "geometry": {
+                "type": "LineString",
+                "coordinates": coordinates
+            }
+        })
+    return features
+
+
+def features_as_geojson_features(features):
+    """
+    Converts a list of features into a list of geojson features
+    :param features: 
+    :return: 
+    """
+    parts = []
+    for feature in features:
+        part = {
+            "type": "Feature",
+            "properties": {},
+            "geometry": json.loads(feature.geometry().asJson())
+        }
+        parts.append(part)
+
+    return parts
---
+++ impact/routing_api.py
@@ -0,0 +1,125 @@
+import json
+
+from . import transform_layer_to_WGS84, extract_valid_geometries, route_meta, lat_lon_coor, \
+    fetch_non_blocking, extract_coordinates_array, staging_mode
+
+
+# Initialize Qt resources from file resources.py
+# Import the code for the dialog
+
+
+class routing_api(object):
+    default_profiles = ["car", "car.shortest", "car.opa", "car.default", "car.classifications",
+                        "car.classifications_aggressive", "pedestrian", "pedestrian.shortest", "pedestrian.default",
+                        "pedestrian.opa",
+                        "bicycle", "bicycle.fastest", "bicycle.shortest", "bicycle.safety", "bicycle.comfort",
+                        "bicycle.comfort_safety",
+                        "bicycle.electrical_fastest", "bicycle.networks", "bicycle.brussels", "bicycle.genk",
+                        "bicycle.antwerp",
+                        "bicycle.cycle_highway", "bicycle.node_network", "bicycle.commute", "bicycle.b2w",
+                        "bicycle.anyways_network"]
+
+    def __init__(self, api_key, baseurl=None, is_impact_backend=False,
+                 auth_token=None):
+        
+        if(baseurl == None):
+            if staging_mode:
+                baseurl = "https://staging.anyways.eu/routing-api"
+            else:
+                baseurl="https://routing.anyways.eu/api"
+        
+        
+        self._api_key = api_key
+        self._baseurl = baseurl
+        self.is_impact_backend = is_impact_backend
+        self.loaded_explanations = None
+        self.auth_token = auth_token
+
+    def construct_url(self, fromCoor, toCoor, profile):
+        """
+        Constructs the correct URL for a 'from to'-query
+        :param fromCoor: 
+        :param toCoor: 
+        :param profile: 
+        :return: 
+        """
+        if self.is_impact_backend:
+            return self._baseurl + "/routing?apiKey=" + self._api_key + "&profile=" + profile + "&loc=" + lat_lon_coor(
+                fromCoor) + "&loc=" + lat_lon_coor(toCoor)
+        return self._baseurl + "/v1/routes?apiKey=" + self._api_key + "&loc=" + fromCoor + "&loc=" + toCoor + "&profile=" + profile
+
+    def _request_json(self, url, callback, onfail):
+        fetch_non_blocking(url, lambda str: callback(json.loads(str)), onfail)
+
+    def request_supported_profiles(self, withDescriptions):
+        """
+        Fetches all (public) profiles. Legacy/hidden profiles are filtered
+        :return: (name or aliasname --> description, names)
+        """
+
+        def callback(raw):
+            profiles = json.loads(raw)["profiles"]
+            descriptions = {}
+            description_keys = []
+            for profile in profiles:
+                type = profile["type"]  # eg "car", "bicycle", "pedestrian"
+                name = profile["name"]  # eg "shortest", "fastest"
+                description = profile["description"]
+                descriptions[type + "." + name] = description
+                if not description.lower().startswith("[legacy]"):
+                    description_keys.append(type + "." + name)
+                for alias in profile["aliases"]:
+                    descriptions[alias] = description
+            description_keys.sort()
+            withDescriptions(descriptions, description_keys)
+
+        url = self._baseurl + "/v1/profiles"
+        fetch_non_blocking(url, callback, lambda err: print("Could not load profiles due to" + err))
+
+    def patch_feature(self, feature):
+        """
+        Adds a determnistic GUID to the feature
+        """
+
+        def clean_coord(coord):
+            # Only keep the first two coordinates; a height might be provided too, but this breaks the GUID
+            coord = coord[0:2]
+            return ",".join(map(lambda c: str(round(c, 8)), coord))
+
+        props = feature['properties']
+        if feature["geometry"]["type"] == "LineString":
+            coords = feature["geometry"]["coordinates"]
+            startp = clean_coord(coords[0])
+            endp = clean_coord(coords[1])
+            props["guid"] = startp + ";" + endp
+
+    def request_all_routes(self, fromCoors, toCoors, profile, withRoutes, onError):
+        """
+        Reqeusts a matrix call
+        :param fromCoors: [number,number][], containing coordinates in "lon,lat" format
+        :param toCoors:  [number,number][], containing coordinates in "lon,lat" format
+        :param profile: string, the name of the profile to use
+        :param: witRoutes: callback which is called with a route_meta[][] object when the call is finished
+        :return: route_meta[][], where route_meta[x][y].fromCoor == fromCoors[x] and route_meta[x][y].toCoor == toCoors[y]
+        """
+
+        def callback(raw):
+            data = json.loads(raw)
+            withRoutes(data)
+            print("Got data")
+
+        if self.is_impact_backend:
+            url = self._baseurl + "/routing/many-to-many"
+        else:
+            url = self._baseurl + "/v1.1/matrix"
+        print(url)
+        headers = {}
+        if self.auth_token is not None:
+            headers = {
+                "Authorization": self.auth_token
+            }
+        fetch_non_blocking(url, callback, onError, postData={
+            "profile": profile,
+            "from": fromCoors,
+            "to": toCoors
+        }, headers=headers)
