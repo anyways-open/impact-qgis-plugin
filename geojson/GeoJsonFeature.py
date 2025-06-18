731179c576aa023427821be33fd3e8bfa32888c8
xivk <ben@anyways.eu>
2025-06-18 11:09:00 +0200

    Fixed issue with partial segments.

--- geojson/GeoJsonFeature.py
+++ geojson/GeoJsonFeature.py
@@ -1,4 +1,10 @@
 import copy
+import math
+from typing import Union, Any
+
+from qgis._core import QgsMessageLog, Qgis
+
+from ..settings import MESSAGE_CATEGORY
 
 
 class GeoJsonFeature(object):
@@ -6,7 +12,7 @@ class GeoJsonFeature(object):
         self.feature = feature
 
     @staticmethod
-    def to_feature_collection(features: list['GeoJsonFeature']) -> []:
+    def to_feature_collection(features: list['GeoJsonFeature']) -> dict[str, Union[str, list[Any]]]:
         raw_features = []
         for feature in features:
             raw_features.append(feature.feature)
@@ -30,6 +36,82 @@ class GeoJsonFeature(object):
             "geometry": {
                 "type": "LineString",
                 "coordinates": reversed_coordinates
+            }
+        })
+
+    def get_length(self):
+        length = 0.0
+        coordinates: list[list[float]] = self.feature["geometry"]["coordinates"]
+
+        for c in range(1, len(coordinates)):
+            length += math.sqrt (math.pow(coordinates[c-1][0] - coordinates[c][0], 2) + math.pow(coordinates[c-1][1] - coordinates[c][1], 2))
+
+        return length
+
+    def get_cut(self, tail_offset: int, head_offset: int) -> 'GeoJsonFeature':
+        length = self.get_length()
+
+        tail = length * (tail_offset / 65535.0)
+        head = length * (head_offset / 65535.0)
+        QgsMessageLog.logMessage(f"{length} - {tail} - {head}", MESSAGE_CATEGORY, Qgis.Info)
+
+        if tail > head:
+            raise ValueError("tail offset is greater than head offset")
+
+        coordinates: list[list[float]] = self.feature["geometry"]["coordinates"]
+        cut_coordinates: list[list[float]] = []
+
+        current = 0.0
+        for c in range(1, len(coordinates)):
+            x_diff = coordinates[c - 1][0] - coordinates[c][0]
+            y_diff = coordinates[c - 1][1] - coordinates[c][1]
+            segment_length = math.sqrt(math.pow(x_diff, 2) + math.pow(y_diff, 2))
+            if segment_length <= 0:
+                continue
+
+            n = current + segment_length
+            QgsMessageLog.logMessage(f"STEP {c}/{len(coordinates)}@{len(cut_coordinates)} - {current} - {segment_length}", MESSAGE_CATEGORY, Qgis.Info)
+
+            if len(cut_coordinates) <= 0:
+                if n >= tail:
+                    if tail <= 0:
+                        cut_coordinates.append(coordinates[c-1])
+                    else:
+                        tail_segment_offset = (n - tail) / segment_length
+                        if tail_segment_offset <= 0:
+                            cut_coordinates.append(coordinates[c-1])
+                        else:
+                            interpolated = [coordinates[c][0] + (x_diff * tail_segment_offset),coordinates[c][1] + (y_diff * tail_segment_offset)]
+                            cut_coordinates.append(interpolated)
+                    QgsMessageLog.logMessage(f"STEP {c}/{len(coordinates)}@{len(cut_coordinates)} - TAIL FOUND", MESSAGE_CATEGORY, Qgis.Info)
+
+            if len(cut_coordinates) > 0:
+                if n <= head:
+                    cut_coordinates.append(coordinates[c])
+                    QgsMessageLog.logMessage(f"STEP {c}/{len(coordinates)}@{len(cut_coordinates)} - INCLUDED", MESSAGE_CATEGORY, Qgis.Info)
+                if n == head:
+                    QgsMessageLog.logMessage(f"STEP {c}/{len(coordinates)}@{len(cut_coordinates)} - EXACT HEAD FOUND", MESSAGE_CATEGORY, Qgis.Info)
+                    break
+                if n > head:
+                    head_segment_offset = (n - head) / segment_length
+                    interpolated = [coordinates[c][0] + (x_diff * head_segment_offset),coordinates[c][1] + (y_diff * head_segment_offset)]
+                    cut_coordinates.append(interpolated)
+                    QgsMessageLog.logMessage(f"STEP {c}/{len(coordinates)}@{len(cut_coordinates)} - HEAD FOUND", MESSAGE_CATEGORY, Qgis.Info)
+                    break
+
+            current = n
+
+        properties = copy.deepcopy(self.feature["properties"])
+
+        if len(cut_coordinates) <= 1:
+            raise ValueError("cut is empty or only has one coordinate")
+
+        return GeoJsonFeature({
+            "type": "Feature",
+            "properties": properties,
+            "geometry": {
+                "type": "LineString",
+                "coordinates": cut_coordinates
             }
         })
 
--- layers/SegmentsLayerBuilder.py
+++ layers/SegmentsLayerBuilder.py
@@ -1,5 +1,6 @@
-from qgis._core import QgsVectorLayer
+from qgis._core import QgsVectorLayer, QgsMessageLog, Qgis
 
+from ..settings import MESSAGE_CATEGORY
 from ..geojson.GeoJsonFeature import GeoJsonFeature
 from ..routing.tasks.RouteResult import RouteResult
 from ..routing.Matrix import Matrix
@@ -12,6 +13,29 @@ class SegmentsLayerBuilder(object):
         self.results = results
 
     def build_layer(self, project_path: str) -> QgsVectorLayer:
+        cuts_per_segment: dict[str, list[int]] = dict()
+        for i, result in enumerate(self.results):
+            if not result.is_success():
+                continue
+
+            response = result.result
+            for route_row in response.routes:
+                for alternatives in route_row:
+                    for route in alternatives:
+                        for route_segment in route.segments:
+                            segment_key = f'{route_segment.global_id}-{route_segment.forward}'
+                            segment_cuts: list[int] = []
+                            if segment_key in cuts_per_segment:
+                                segment_cuts = cuts_per_segment[segment_key]
+
+                            if route_segment.tail_offset not in segment_cuts:
+                                segment_cuts.append(route_segment.tail_offset)
+                                segment_cuts = sorted(segment_cuts)
+                            if route_segment.head_offset not in segment_cuts:
+                                segment_cuts.append(route_segment.head_offset)
+                                segment_cuts = sorted(segment_cuts)
+
+                            cuts_per_segment[segment_key] = segment_cuts
 
         # QgsMessageLog.logMessage(f"{flattened}", MESSAGE_CATEGORY, Qgis.Info)
         histogram: dict[str, GeoJsonFeature] = dict()
@@ -29,20 +53,42 @@ class SegmentsLayerBuilder(object):
                         for route_segment in route.segments:
                             segment_key = f'{route_segment.global_id}-{route_segment.forward}'
 
-                            if segment_key not in histogram:
-                                segment = response.segments[route_segment.global_id]
-                                if segment is None:
-                                    raise RuntimeError(f"Invalid response: could not find segment {route_segment.global_id}")
+                            # get the cuts covered by this route_segment
+                            segment_cuts = cuts_per_segment[segment_key]
+                            covered_cuts: list[int] = []
+                            for segment_cut in segment_cuts:
+                                if route_segment.tail_offset > segment_cut:
+                                    continue
+                                if route_segment.head_offset < segment_cut:
+                                    continue
 
-                                if not route_segment.forward:
-                                    segment = GeoJsonFeature.reverse_linestring(segment)
+                                covered_cuts.append(segment_cut)
 
-                                histogram[segment_key] = segment
-                            else:
-                                 segment = histogram[segment_key]
+                            # process the segment for each cut
+                            for k in range(1, len(covered_cuts)):
+                                tail_offset = covered_cuts[k - 1]
+                                head_offset = covered_cuts[k]
+                                segment_cut_key = f'{route_segment.global_id}-{route_segment.forward}@{tail_offset}-{head_offset}'
 
-                            segment.add_to_attribute_value("count", count_per_alternative)
-                            segment.add_or_update_attribute("id", segment_key)
+                                if segment_cut_key not in histogram:
+                                    segment = response.segments[route_segment.global_id]
+                                    if segment is None:
+                                        raise RuntimeError(f"Invalid response: could not find segment {route_segment.global_id}")
+
+                                    if not route_segment.forward:
+                                        segment = GeoJsonFeature.reverse_linestring(segment)
+
+                                    if tail_offset != 0 or head_offset != 65535:
+                                        QgsMessageLog.logMessage(f"{segment_cut_key}", MESSAGE_CATEGORY, Qgis.Info)
+                                        QgsMessageLog.logMessage(f"{segment}", MESSAGE_CATEGORY, Qgis.Info)
+                                        segment = segment.get_cut(tail_offset, head_offset)
+
+                                    histogram[segment_cut_key] = segment
+                                else:
+                                     segment = histogram[segment_cut_key]
+
+                                segment.add_to_attribute_value("count", count_per_alternative)
+                                segment.add_or_update_attribute("id", segment_cut_key)
 
             #QgsMessageLog.logMessage(f"{segment_guid}{segment_forward}: {result}", MESSAGE_CATEGORY, Qgis.Info)
 
